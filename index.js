985dfc03b70de598d7fd673217e4906cd9649b81
Aspen James <aspenjames@tqca.org>
2019-03-13 22:09:53 -0700

    each and map

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for(i in collection) {
+        callback(collection[i])
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArr = []
+      for(i in collection) {
+        newArr.push(callback(collection[i]))
+      }
+      return newArr
     },
 
-    reduce: function() {
+    reduce: function(collection, callback, acc) {
 
     },
     
